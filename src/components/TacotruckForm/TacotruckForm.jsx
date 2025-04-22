5ce65fcd4c014f8419687716bad7f6b51fcdca51
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-21 20:36:57 -0700

    wip: fixed form submit and it gets it in the backend

--- src/components/TacotruckForm/TacotruckForm.jsx
+++ src/components/TacotruckForm/TacotruckForm.jsx
@@ -4,21 +4,24 @@ import { useState, useEffect } from 'react';
 import * as tacotruckService from '../../services/tacotruckService';
 
 const TacotruckForm = (props) => {
-     // Destructure tacotruckId from the useParams hook, and console log it
+  // Destructure tacotruckId from the useParams hook, and console log it
   const { tacotruckId } = useParams();
   console.log(tacotruckId);
   const [formData, setFormData] = useState({
-    title: '',
-    text: '',
+    // title: '',
+    // text: '',
+    name: '',
+    location: '',
+    description: '',
     // category: 'News',
   });
 
   const handleChange = (evt) => {
+    console.log(`Changing ${evt.target.name} to ${evt.target.value}`);
     setFormData({ ...formData, [evt.target.name]: evt.target.value });
   };
 
-
-const handleSubmit = (evt) => {
+  const handleSubmit = (evt) => {
     evt.preventDefault();
     console.log('Submitting form with:', formData); // ✅ Add this
     if (tacotruckId) {
@@ -27,7 +30,7 @@ const handleSubmit = (evt) => {
       props.handleAddTacotruck(formData);
     }
   };
-  
+
 
   useEffect(() => {
     const fetchTacotruck = async () => {
@@ -35,8 +38,11 @@ const handleSubmit = (evt) => {
       setFormData(tacotruckData);
     };
     if (tacotruckId) fetchTacotruck();
-    return () => setFormData({ title: '', text: '', 
+    return () => setFormData({
+      // title: '', 
+      text: '',
       // category: 'News' 
+      name: '', location: '', description: '' 
     });
   }, [tacotruckId]);
 
@@ -45,7 +51,7 @@ const handleSubmit = (evt) => {
     <main>
       <h1>{tacotruckId ? 'Edit Tacotruck' : 'New Tacotruck'}</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor='title-input'>Title</label>
+        {/* <label htmlFor='title-input'>Title</label>
         <input
           required
           type='text'
@@ -71,13 +77,51 @@ const handleSubmit = (evt) => {
           value={formData.category}
           onChange={handleChange}
         > */}
-          {/* <option value='News'>News</option>
+        {/* <option value='News'>News</option>
           <option value='Games'>Games</option>
           <option value='Music'>Music</option>
           <option value='Movies'>Movies</option>
           <option value='Sports'>Sports</option>
           <option value='Television'>Television</option> */}
         {/* </select> */}
+        <label htmlFor='name-input'>Name</label>
+        <input
+          required
+          type='text'
+          name='name'
+          id='name-input'
+          value={formData.name}
+          onChange={handleChange}
+        />
+
+        <label htmlFor='location-input'>Location</label>
+        <input
+          required
+          type='text'
+          name='location'
+          id='location-input'
+          value={formData.location}
+          onChange={handleChange}
+        />
+
+        <label htmlFor='description-input'>Description</label>
+        <textarea
+          required
+          name='description'
+          id='description-input'
+          value={formData.description}
+          onChange={handleChange}
+        />
+        <label htmlFor='text-input'>Text</label>
+        <textarea
+          required
+          type='text'
+          name='text'
+          id='text-input'
+          value={formData.text}
+          onChange={handleChange}
+        />
+
         <button type='submit'>SUBMIT</button>
       </form>
     </main>
